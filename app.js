68810842f8b8cb11c1a94af11b65539ef849d724
Amidou Diallo <diallo.amidou24@yahoo.fr>
2025-03-20 23:37:22 +0100

    on ajoute le domaine vercel

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const app = express();
 // Configuration CORS
 app.use(
     cors({
-        origin: ["http://localhost:4000", "http://localhost:3001"],
+        origin: ["http://localhost:4000", "http://localhost:3001", "https://frontend-spotify-eight.vercel.app"],
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
         allowedHeaders: ["Content-Type", "Authorization"],
         credentials: true,
